2282422062fea9f6a3c27182c5b1da67d7969897
Dmitry <43745268+DKYOO@users.noreply.github.com>
2022-04-29 14:08:12 +0300

    Update CoinManager.swift
    Kek

--- BitCoinValue/Network/CoinManager.swift
+++ BitCoinValue/Network/CoinManager.swift
@@ -21,7 +21,7 @@ struct CoinManager {
 	
 	let baseURL  = "https://rest.coinapi.io/v1/exchangerate/BTC"
 	
-	private let apiKey = "Yor API Key here"
+	private let apiKey = "Your API Key here"
 	
 	let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 	
